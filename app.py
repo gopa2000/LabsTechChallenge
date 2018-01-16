dfeac9a5c9a10c457b95a8cd2e6bd21b042fc5b9
gopa2000 <doberoi96@gmail.com>
2018-01-16 11:34:55 -0500

    added route /do2324

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/do2324', methods=['GET'])
+def homepage():
+	return render_template('do2324.html')
 
 if __name__ == '__main__':
     app.run()
